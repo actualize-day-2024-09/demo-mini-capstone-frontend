cf78f5f17436d48e80149f2bfa4cd79608226a38
Peter Jang <peter.x.jang@gmail.com>
2024-12-11 14:45:20 -0600

    Display shopping cart info

--- src/CartedProductsIndexPage.jsx
+++ src/CartedProductsIndexPage.jsx
@@ -6,6 +6,12 @@ export function CartedProductsIndexPage() {
   return (
     <div>
       <h1>Shopping Cart ({cartedProducts.length} total)</h1>
+      {cartedProducts.map((cartedProduct) => (
+        <div key={cartedProduct.id}>
+          <h2>{cartedProduct.product.name}</h2>
+          <p>Quantity: {cartedProduct.quantity}</p>
+        </div>
+      ))}
     </div>
   );
 }
