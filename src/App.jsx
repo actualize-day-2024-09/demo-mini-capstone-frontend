56d30b5b80e9475a04ddb0013a1daeaef065a40f
Peter Jang <peter.x.jang@gmail.com>
2024-12-11 14:29:16 -0600

    Install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { SignupPage } from "./SignupPage";
 import { LoginPage } from "./LoginPage";
@@ -8,16 +9,34 @@ import { Footer } from "./Footer";
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <SignupPage />
-      <LoginPage />
-      <ProductsPage />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a> | <LogoutLink />
+        <Link to="/">Home</Link> | <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link> | <LogoutLink />
       </nav>
     </header>
   );
