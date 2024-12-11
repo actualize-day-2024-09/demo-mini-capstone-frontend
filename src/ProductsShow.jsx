c8eac9cd18dd4d56cd553b17d3253e0f5beafa58
Peter Jang <peter.x.jang@gmail.com>
2024-12-11 14:56:09 -0600

    Add a form to add items to shopping cart

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,8 +1,14 @@
-export function ProductsShow({ product, onUpdate, onDestroy }) {
+export function ProductsShow({ product, onUpdate, onDestroy, onAddToCart }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
     onUpdate(product, params, () => event.target.reset());
+  };
+
+  const handleShoppingCartSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    onAddToCart(params);
   };
 
   return (
@@ -23,6 +29,14 @@ export function ProductsShow({ product, onUpdate, onDestroy }) {
         <button type="submit">Update</button>
       </form>
       <button onClick={() => onDestroy(product)}>Destroy</button>
+
+      <form onSubmit={handleShoppingCartSubmit}>
+        <div>
+          Quantity: <input name="quantity" type="number" />
+        </div>
+        <input name="product_id" type="hidden" value={product.id} />
+        <button type="submit">Add to cart</button>
+      </form>
     </div>
   );
 }
