01cfc7fde6d070dd2833c38aaee6bd9bf1042608
Peter Jang <peter.x.jang@gmail.com>
2024-12-12 20:17:12 -0600

    Style shopping cart page

--- src/CartedProductsIndexPage.jsx
+++ src/CartedProductsIndexPage.jsx
@@ -14,14 +14,16 @@ export function CartedProductsIndexPage() {
 
   return (
     <div>
-      <h1>Shopping Cart ({cartedProducts.length} total)</h1>
+      <h1 className="text-3xl font-bold mb-3">Shopping Cart ({cartedProducts.length} total)</h1>
       {cartedProducts.map((cartedProduct) => (
-        <div key={cartedProduct.id}>
-          <h2>{cartedProduct.product.name}</h2>
+        <div key={cartedProduct.id} className="mb-4">
+          <h2 className="text-2xl font-bold mb-3">{cartedProduct.product.name}</h2>
           <p>Quantity: {cartedProduct.quantity}</p>
         </div>
       ))}
-      <button onClick={handleClick}>Buy now</button>
+      <button onClick={handleClick} className="rounded border border-gray-300 p-2 mt-2 hover:bg-gray-100">
+        Buy now
+      </button>
     </div>
   );
 }
