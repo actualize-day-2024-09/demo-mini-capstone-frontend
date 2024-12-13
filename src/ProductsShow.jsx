de8445936b6d1b53b2f36c96bf58fa06e4b9e0b2
Peter Jang <peter.x.jang@gmail.com>
2024-12-12 20:03:17 -0600

    Style products show page

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -13,30 +13,70 @@ export function ProductsShow({ product, onUpdate, onDestroy, onAddToCart }) {
 
   return (
     <div>
-      <h1>{product.name}</h1>
-      <p>Price: {product.price}</p>
-      <p>Description: {product.description}</p>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input name="name" defaultValue={product.name} type="text" />
+      <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+      <div className="grid grid-rows-3 grid-flow-col gap-4">
+        <div className="row-span-3">
+          <img src={product.primary_image_url} className="w-full rounded" />
+          <form onSubmit={handleShoppingCartSubmit} className="mt-4 p-2 rounded bg-slate-200">
+            <div>
+              <label className="font-bold">Quantity:</label>
+              <input name="quantity" type="number" className="mt-1 block w-full rounded-md border-gray-300" />
+            </div>
+            <input name="product_id" type="hidden" value={product.id} />
+            <button type="submit" className="rounded border border-gray-300 p-2 mt-2 hover:bg-gray-100">
+              Add to cart
+            </button>
+          </form>
         </div>
-        <div>
-          Price: <input name="price" defaultValue={product.price} type="text" />
-        </div>
-        <div>
-          Description: <input name="description" defaultValue={product.description} type="text" />
-        </div>
-        <button type="submit">Update</button>
-      </form>
-      <button onClick={() => onDestroy(product)}>Destroy</button>
+        <div className="col-span-2 ...">
+          <p>
+            <span className="text-2xl">Price:</span> {product.price}
+          </p>
+          <p>
+            <span className="text-2xl">Description:</span> {product.description}
+          </p>
 
-      <form onSubmit={handleShoppingCartSubmit}>
-        <div>
-          Quantity: <input name="quantity" type="number" />
+          <form onSubmit={handleSubmit} className="mt-6">
+            <div className="mb-4">
+              <label className="font-bold">Name:</label>
+              <input
+                name="name"
+                defaultValue={product.name}
+                type="text"
+                className="mt-1 block w-full rounded-md border-gray-300"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="font-bold">Price:</label>
+              <input
+                name="price"
+                defaultValue={product.price}
+                type="text"
+                className="mt-1 block w-full rounded-md border-gray-300"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="font-bold">Description:</label>
+              <input
+                name="description"
+                defaultValue={product.description}
+                type="text"
+                className="mt-1 block w-full rounded-md border-gray-300"
+              />
+            </div>
+            <button type="submit" className="rounded border border-gray-300 p-2 mt-2 hover:bg-gray-100">
+              Update
+            </button>
+            <button
+              onClick={() => onDestroy(product)}
+              type="button"
+              className="rounded border border-gray-300 p-2 mt-2 ml-2 hover:bg-gray-100"
+            >
+              Destroy
+            </button>
+          </form>
         </div>
-        <input name="product_id" type="hidden" value={product.id} />
-        <button type="submit">Add to cart</button>
-      </form>
+      </div>
     </div>
   );
 }
