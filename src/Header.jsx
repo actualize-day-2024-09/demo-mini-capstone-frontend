d4073367f56db00441a960c9e9afcb7d320617bc
Peter Jang <peter.x.jang@gmail.com>
2024-12-12 20:31:57 -0600

    Show links based on login state

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,22 +11,31 @@ export function Header() {
         <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/products">
           All products
         </Link>
-        <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/products/new">
-          New product
-        </Link>
-        <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/carted_products">
-          Shopping Cart
-        </Link>
-        <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/orders">
-          Orders
-        </Link>
-        <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/signup">
-          Signup
-        </Link>
-        <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/login">
-          Login
-        </Link>
-        <LogoutLink className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" />
+        {localStorage.email ? (
+          <>
+            <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/products/new">
+              New product
+            </Link>
+            <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/carted_products">
+              Shopping Cart
+            </Link>
+            <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/orders">
+              Orders
+            </Link>
+
+            <LogoutLink className="p-2 mr-2 rounded-md outline-slate-700 outline hover:bg-slate-200 hover:text-black" />
+          </>
+        ) : (
+          <>
+            <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/signup">
+              Signup
+            </Link>
+            <Link className="p-2 mr-2 rounded-md hover:bg-slate-200 hover:text-black" to="/login">
+              Login
+            </Link>
+          </>
+        )}
+        <span className="float-end">{localStorage.email}</span>
       </nav>
     </header>
   );
