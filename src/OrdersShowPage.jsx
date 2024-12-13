64e6049f00d7645c044cebfbd10e1096d663cfdd
Peter Jang <peter.x.jang@gmail.com>
2024-12-12 20:20:40 -0600

    Style orders show page

--- src/OrdersShowPage.jsx
+++ src/OrdersShowPage.jsx
@@ -6,19 +6,22 @@ export function OrdersShowPage() {
 
   return (
     <div>
-      <h2>Order #{order.id}</h2>
+      <h2 className="text-3xl font-bold mb-4">Order #{order.id}</h2>
       {order.carted_products.map((carted_product) => (
         <div key={carted_product.id}>
-          <h4>
+          <h4 className="font-bold mb-2">
             {carted_product.quantity} x {carted_product.product.name}
           </h4>
         </div>
       ))}
-      <hr />
-      <p>Subtotal: {order.subtotal}</p>
-      <p>Tax: {order.tax}</p>
-      <p>Total: {order.total}</p>
-      <Link to="/orders">All orders</Link>
+      <div className="bg-slate-300 rounded p-4 mb-10 mt-4">
+        <p>Subtotal: {order.subtotal}</p>
+        <p>Tax: {order.tax}</p>
+        <p>Total: {order.total}</p>
+      </div>
+      <Link to="/orders" className="rounded border border-gray-300 p-2 hover:bg-gray-100">
+        All orders
+      </Link>
     </div>
   );
 }
