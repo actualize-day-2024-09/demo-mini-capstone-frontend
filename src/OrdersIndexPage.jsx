115bed2d95a2c03c84868389808a169ad8301f3c
Peter Jang <peter.x.jang@gmail.com>
2024-12-12 20:23:28 -0600

    Style orders index page

--- src/OrdersIndexPage.jsx
+++ src/OrdersIndexPage.jsx
@@ -7,8 +7,10 @@ export function OrdersIndexPage() {
     <div>
       {orders.map((order) => (
         <div key={order.id}>
-          <h2>Order #{order.id}</h2>
-          <Link to={`/orders/${order.id}`}>View more</Link>
+          <h2 className="text-2xl font-bold mb-3 mt-6">Order #{order.id}</h2>
+          <Link to={`/orders/${order.id}`} className="rounded border border-gray-300 p-2 mt-2 hover:bg-gray-100">
+            View more
+          </Link>
         </div>
       ))}
     </div>
