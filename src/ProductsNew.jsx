d5321edf4dab2ec5584d19cb76e2d2d2fdb369c9
Peter Jang <peter.x.jang@gmail.com>
2025-01-09 13:20:51 -0600

    Replace input with dropdown to make it more user friendly

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export function ProductsNew({ onCreate }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
     onCreate(params, () => event.target.reset());
   };
+
+  const [suppliers, setSuppliers] = useState([]);
+
+  const handleSuppliersIndex = () => {
+    axios.get("/suppliers.json").then((response) => {
+      console.log(response);
+      setSuppliers(response.data);
+    });
+  };
+
+  useEffect(handleSuppliersIndex, []);
 
   return (
     <div>
@@ -22,7 +36,17 @@ export function ProductsNew({ onCreate }) {
           Image url: <input name="image_url" type="text" className="mt-1 block w-full rounded-md border-gray-300" />
         </div>
         <div className="mb-2">
-          Supplier id: <input name="supplier_id" type="text" className="mt-1 block w-full rounded-md border-gray-300" />
+          Supplier:
+          <select name="supplier_id" className="mt-1 block w-full rounded-md border-gray-300" defaultValue="">
+            <option value="" disabled>
+              Select a supplier
+            </option>
+            {suppliers.map((supplier) => (
+              <option key={supplier.id} value={supplier.id}>
+                {supplier.name}
+              </option>
+            ))}
+          </select>
         </div>
         <button type="submit" className="rounded border border-gray-300 p-2 mt-2 hover:bg-gray-100">
           Create
